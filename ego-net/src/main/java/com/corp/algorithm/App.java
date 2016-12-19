970c8040dead64237ac3e85ec0740f638915ce7f
yi xu <yi xu>
2016-12-19 22:37:43 +0800

    add spark init

--- ego-net/src/main/java/com/corp/algorithm/App.java
+++ ego-net/src/main/java/com/corp/algorithm/App.java
@@ -1,72 +1,92 @@
 package com.corp.algorithm;
 
-import java.io.IOException;
-import java.util.*;
+import scala.Tuple2;
+import scala.Tuple3;
 
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
 
-/**
- * Hello world!
- *
- */
-public class App {
-	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
+public final class App {
+	// private static final String SPACE = " ";
+	private static final String SPILTOR = ",";
+	private static final int PARTIITON_NUM = 10;
 
-		public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
-				throws IOException {
-			String line = value.toString();
-			StringTokenizer tokenizer = new StringTokenizer(line);
-			while (tokenizer.hasMoreTokens()) {
-				word.set(tokenizer.nextToken());
-				output.collect(word, one);
-			}
-		}
-	}
-
-	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
-				Reporter reporter) throws IOException {
-			int sum = 0;
-			while (values.hasNext()) {
-				sum += values.next().get();
-			}
-			output.collect(key, new IntWritable(sum));
-		}
+	public static int caculateHashcode(int number) {
+		return number % PARTIITON_NUM;
 	}
 
 	public static void main(String[] args) throws Exception {
-		JobConf conf = new JobConf(App.class);
-		conf.setJobName("wordcount");
-//		conf.set("", "");
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
+		JavaSparkContext context = new JavaSparkContext(new SparkConf().setAppName("word count"));
+		JavaRDD<String> textFile = context.textFile("LICENSE");
 
-		conf.setMapperClass(Map.class);
-		// conf.setCombinerClass(Reduce.class);
-		conf.setReducerClass(Reduce.class);
+		JavaPairRDD<Tuple3<Integer, Integer, Integer>, Tuple2<Integer, Integer>> inputGraph = textFile
+				.mapToPair(new PairFunction<String, Tuple3<Integer, Integer, Integer>, Tuple2<Integer, Integer>>() {
 
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
+					@Override
+					public Tuple2<Tuple3<Integer, Integer, Integer>, Tuple2<Integer, Integer>> call(String line) throws Exception {
+						String values[] = line.split(SPILTOR);
+						int edge1 = Integer.parseInt(values[0]);
+						int edge2 = Integer.parseInt(values[1]);
+						int i = caculateHashcode(edge1);
+						int j = caculateHashcode(edge2);
+						if (i == j) {
+							for (int z = 1; z <= PARTIITON_NUM; z++) {
+								if (z == i)
+									continue;
+								for (int w = 1; w <= PARTIITON_NUM; w++) {
+									if (w == i || w == z)
+										continue;
+									return new Tuple2<Tuple3<Integer, Integer, Integer>, Tuple2<Integer, Integer>>(
+											new Tuple3<Integer, Integer, Integer>(i, z, w),
+											new Tuple2<Integer, Integer>(edge1, edge2));
+								}
+							}
+						} else {
+							for (int z = 1; z <= PARTIITON_NUM; z++) {
+								if (z == i || z == j)
+									continue;
+								return new Tuple2<Tuple3<Integer, Integer, Integer>, Tuple2<Integer, Integer>>(
+										new Tuple3<Integer, Integer, Integer>(i, j, z),
+										new Tuple2<Integer, Integer>(edge1, edge2));
+							}
+						}
+						return null;
+					}
 
-		FileInputFormat.setInputPaths(conf, new Path("LICENSE"));
-		FileOutputFormat.setOutputPath(conf, new Path("out"));
+				});
 
-		JobClient.runJob(conf);
+		// JavaRDD<String> words = textFile.flatMap(new FlatMapFunction<String,
+		// String>() {
+		//
+		// @Override
+		// public Iterator<String> call(String s) throws Exception {
+		// return Arrays.asList(s.split(SPACE)).iterator();
+		// }
+		//
+		// });
+		//
+		//
+		// JavaPairRDD<String, Integer> pairs = words.mapToPair(new
+		// PairFunction<String, String, Integer>() {
+		// public Tuple2<String, Integer> call(String s) {
+		// return new Tuple2<String, Integer>(s, 1);
+		// }
+		// });
+		// JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new
+		// Function2<Integer, Integer, Integer>() {
+		// public Integer call(Integer a, Integer b) {
+		// return a + b;
+		// }
+		// });
+		// counts.saveAsTextFile("out");
+		//
+		// List<Tuple2<String, Integer>> output = counts.collect();
+		// for (Tuple2<?, ?> tuple : output) {
+		// System.out.println(tuple._1() + ": " + tuple._2());
+		// }
+		context.stop();
 	}
 }
